4007b94dcffddf9cd673db64ce99098dbb9b8df7
Jordan Schneidler <jordan.schneidler@gmail.com>
2019-11-12 22:25:50 -0600

    WIP. Integer overflow issue.

--- mandelbrotlib/src/mandelbrot.cpp
+++ mandelbrotlib/src/mandelbrot.cpp
@@ -1,5 +1,6 @@
 #include "mandelbrot.hpp"
 #include <iostream>
+#include <cmath>
 
 namespace Mandelbrot
 {
@@ -26,7 +27,24 @@ namespace Mandelbrot
 		double increment = scale / resolution;
 
 		// Get index of center cell
-		uint64_t center = (resolution / 2) - 1;
+		uint64_t center_cell = ceil(resolution / 2.0) - 1;
+
+		std::cout << resolution << std::endl;
+		std::cout << increment << std::endl;
+		std::cout << center_cell << std::endl;
+
+		double imaginary, real;
+		for (uint64_t i = 0; i < resolution; i++)
+		{
+			imaginary = center.getImaginary() + ((center_cell - i) * increment);
+			for (uint64_t j = 0; j < resolution; j++)
+			{
+				real = center.getReal() - ((center_cell - j) * increment);
+				grid.at(i).at(j) = iterations(max_iterations, ComplexNumber(real, imaginary), threshold);
+				printf("(%.2f, %.2fi) ", real, imaginary);
+			}
+			std::cout << std::endl;
+		}
 
 		return grid;
 	}
--- tests/mandelbrot.cpp
+++ tests/mandelbrot.cpp
@@ -2,6 +2,8 @@
 #include "mandelbrot.hpp"
 #include "complex_number.hpp"
 
+void printGrid (t_mandelbrot_grid grid);
+
 TEST(MandelbrotTest, iterations)
 {
     ASSERT_EQ(Mandelbrot::iterations(5, ComplexNumber{-1}, 2), 0);
@@ -11,11 +13,28 @@ TEST(MandelbrotTest, iterations)
     ASSERT_EQ(Mandelbrot::iterations(5, ComplexNumber{-1, 1}, 2), 3);
 }
 
-TEST(MandelbrotTest, execute)
+TEST(MandelbrotTest, evaluate)
 {
-	t_mandelbrot_grid grid = Mandelbrot::evaluate(2, 5, ComplexNumber(), 1, 5);
+    const uint64_t resolution = 5;
+	t_mandelbrot_grid grid = Mandelbrot::evaluate(2, 5, ComplexNumber{5, 3}, 10, resolution);
 
 	// Check size of grid
-	ASSERT_EQ(grid.size(), 5);
-	ASSERT_EQ(grid.at(0).size(), 5);
+	ASSERT_EQ(grid.size(), resolution);
+	ASSERT_EQ(grid.at(0).size(), resolution);
+
+    printGrid(grid);
+	// Check grid values
+	// ASSERT_EQ(grid.at(0), 5);
+}
+
+void printGrid (t_mandelbrot_grid grid)
+{
+    for (uint64_t i = 0; i < grid.size(); i++)
+    {
+        for (uint64_t j = 0; j < grid.at(i).size(); j++)
+        {
+            std::cout << grid.at(i).at(j) << " ";
+        }
+        std::cout << std::endl;
+    }
 }
