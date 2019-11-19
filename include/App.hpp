e87b56da0f87b63f19f86147935370fcabf2c028
Jordan Schneidler <jordanschneidler@northwesternmutual.com>
2019-11-19 17:32:02 -0600

    Upped DEFAULT_MAX_ITERATIONS.

--- include/App.hpp
+++ include/App.hpp
@@ -6,7 +6,7 @@
 
 const uint64_t DEFAULT_RESOLUTION = 720;
 const double DEFAULT_THRESHOLD = 2;
-const uint64_t DEFAULT_MAX_ITERATIONS = 50;
+const uint64_t DEFAULT_MAX_ITERATIONS = 100;
 
 const uint16_t LEFT_PANEL_WIDTH = 150;
 
